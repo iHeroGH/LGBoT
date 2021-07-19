7ad14c647333fc991b2aeb02bc49a04a2d68c81f
George Matta <jojomedhat2004@gmail.com>
2021-07-19 01:43:10 -0700

    fixed some formatting

--- utils/sexuality_requester.py
+++ utils/sexuality_requester.py
@@ -8,7 +8,7 @@ class SexualityRequester(object):
     def __init__(self, chosen_sexuality: str):
         self.chosen_sexuality = chosen_sexuality
         self.url = self.get_url()
-        self.soup_list = self.get_soup()
+        self.soup = self.get_soup()
 
     def get_url(self, specific:bool=False):
         """Returns the request URL"""
@@ -18,8 +18,7 @@ class SexualityRequester(object):
         """Webscrapes the LGBT Wiki for the sexualities in the letter group"""
         soup = BeautifulSoup(requests.get(self.url).content, "html.parser")
         soup = soup.find("body").find("div", class_="mw-parser-output").find("a")
-        self.soup = soup
-
+        
         return soup
 
     def get_flag(self):
